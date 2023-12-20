c816ca7ff0a542e5a3e456072f28155ab885e3fd
RamyBit <118202722+RamyBit@users.noreply.github.com>
2023-12-20 11:30:05 +0100

    methods.go edited

--- a-tour-of-go/methods/methods.go
+++ a-tour-of-go/methods/methods.go
@@ -13,7 +13,12 @@ func (v Vertex) Abs() float64 {
 	return math.Sqrt(math.Pow(v.X, 2) + math.Pow(v.Y, 2))
 }
 
+func (v *Vertex) Scale(f float64) {
+	v.X *= f
+	v.Y *= f
+}
 func main() {
 	v := Vertex{2, 3}
+	v.Scale(10)
 	fmt.Println(v.Abs())
 }
