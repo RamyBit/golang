e28aa1a7ff48a15e4f70cd231c58487460426906
RamyBit <118202722+RamyBit@users.noreply.github.com>
2023-12-20 11:58:31 +0100

    interfaces-impl.go added

---
+++ a-tour-of-go/interfaces-impl.go/interfaces-impl.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type I interface {
+	M()
+}
+
+type T struct {
+	S string
+}
+
+func (t T) M() {
+	fmt.Println(t.S)
+}
+
+func main() {
+	var i I
+	t := T{"Hello, World!"}
+	i = t
+	i.M()
+}
